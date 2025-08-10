89b61a247f977a3132047c02b9f6c5e3bbec6831
Rusheel Koushik <koushikgns@gmail.com>
2025-08-10 14:26:33 -0500

    updated app v4

--- lambda_function/app.py
+++ lambda_function/app.py
@@ -4,7 +4,7 @@ import requests
 
 def lambda_handler(event, context):
 
-    print("Deployment via CICD process !! - 08/09/2025 11:09 PM")
+    print("Deployment via CICD process !! - 08/10/2025 2:25 PM")
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     data = response.json()
     
